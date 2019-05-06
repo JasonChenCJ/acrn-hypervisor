0ec4d435fbddfd11fedfd00ba704b678f0477d45
Jason Chen CJ <jason.cj.chen@intel.com>
2019-05-06 21:46:42 +0800

    reshuffle struct vboot_candidates
    struct vboot_candidates is private, so move it into source file, and change it
    to a more suitable name vboot_bootloader_map.
    this patch also add first_vm_boot_mode to indicate if the first vm boot is using
    de-privilege or direct boot mode.
    
    Tracked-On: #1842
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- hypervisor/boot/guest/vboot_wrapper.c
+++ hypervisor/boot/guest/vboot_wrapper.c
@@ -14,7 +14,16 @@
 #include <acpi.h>
 #include "platform_acpi_info.h"
 
+#define NUM_VBOOT_MAP_SUPPORTING 4U
+#define BOOTLOADER_NAME_SIZE 20U
+
+struct vboot_bootloader_map {
+	const char bootloader_name[BOOTLOADER_NAME_SIZE];
+	enum vboot_mode mode;
+};
+
 static struct vboot_operations *vboot_ops;
+static enum vboot_mode first_vm_boot_mode;
 
 /**
  * @pre: this function is called during detect mode which is very early stage,
@@ -26,18 +35,25 @@ void init_vboot_operations(void)
 	struct multiboot_info *mbi;
 	uint32_t i;
 
-	const struct vboot_candidates vboot_candidates[NUM_VBOOT_SUPPORTING] = {
-		{"Slim BootLoader", 15U, get_direct_boot_ops},
-		{"Intel IOTG/TSD ABL", 18U, get_direct_boot_ops},
-		{"ACRN UEFI loader", 16U, get_deprivilege_boot_ops},
-		{"GRUB", 4U, get_direct_boot_ops},
+	const struct vboot_bootloader_map vboot_bootloader_maps[NUM_VBOOT_MAP_SUPPORTING] = {
+		{"Slim BootLoader", DIRECT_BOOT_MODE},
+		{"Intel IOTG/TSD ABL", DIRECT_BOOT_MODE},
+		{"ACRN UEFI loader", DEPRI_BOOT_MODE},
+		{"GRUB", DIRECT_BOOT_MODE},
 	};
 
 	mbi = (struct multiboot_info *)hpa2hva((uint64_t)boot_regs[1]);
-	for (i = 0U; i < NUM_VBOOT_SUPPORTING; i++) {
-		if (strncmp(hpa2hva(mbi->mi_loader_name), vboot_candidates[i].name,
-			vboot_candidates[i].name_sz) == 0) {
-			vboot_ops = vboot_candidates[i].ops();
+	for (i = 0U; i < NUM_VBOOT_MAP_SUPPORTING; i++) {
+		if (strncmp(hpa2hva(mbi->mi_loader_name), vboot_bootloader_maps[i].bootloader_name,
+			strnlen_s(vboot_bootloader_maps[i].bootloader_name, BOOTLOADER_NAME_SIZE)) == 0) {
+			/* Only support two vboot mode */
+			if (vboot_bootloader_maps[i].mode == DEPRI_BOOT_MODE) {
+				vboot_ops = get_deprivilege_boot_ops();
+				first_vm_boot_mode = DEPRI_BOOT_MODE;
+			} else {
+				vboot_ops = get_direct_boot_ops();
+				first_vm_boot_mode = DIRECT_BOOT_MODE;
+			}
 			break;
 		}
 	}
@@ -50,6 +66,12 @@ void init_vboot(void)
 	acpi_fixup();
 #endif
 	vboot_ops->init();
+}
+
+/* @pre: vboot_ops != NULL */
+enum vboot_mode get_first_vm_boot_mode(void)
+{
+	return first_vm_boot_mode;
 }
 
 /* @pre: vboot_ops->get_ap_trampoline != NULL */
